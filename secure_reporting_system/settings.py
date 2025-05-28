a566a621c79586d34fa8657026053fcdf14ddd8a
Godfrey <godfrey.shaunaq.gomes@gmail.com>
2025-05-28 12:50:56 +0400

    database changed to postgres localhost

--- secure_reporting_system/settings.py
+++ secure_reporting_system/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'secure_reporting_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'secure_reporting_database',
+        'PASSWORD': 'shauntiman0307',
+        'HOST': 'localhost',
     }
 }
 
