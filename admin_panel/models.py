4de188bc171dd5108c2a0af30b3aeefdbf6b74d0
Godfrey <godfrey.shaunaq.gomes@gmail.com>
2025-05-28 13:11:24 +0400

    basic user table no auth

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True) #primary key
+    name = models.CharField(max_length=255)
+    email  = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=128) #have to auth
+    designation = models.CharField(max_length=100)
+    institution_id = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
